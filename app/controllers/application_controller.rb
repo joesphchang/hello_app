2511b9d9966f6ebe6f16ea415f55d3d3900f46c8
Joey Chang <joey.chang@id.me>
2022-05-04 11:51:57 -0400

    testing commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
     def hello 
-        render html: "hello world"
+        render html: "this is my world and youre just living in it"
     end
 end
